7dcbfe5e9d318079585d19c298dd55ec48920ada
MateusDiniz <mateus.090998@gmail.com>
2022-06-10 17:45:23 -0300

    falta os bonus

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let flag = 0;
 let precos = [0,0,0]
 let nomes = ["","",""]
 let total = 0;
@@ -13,7 +12,6 @@ function selecionaComida(i, preco, item){
     }
     elementos[x].classList.remove("escondido");
     icon[x].classList.remove("escondido");
-    flag+= 1;
     precos[1] = Number(preco);
     nomes[1] = item;
     fecharPedido();
@@ -29,7 +27,6 @@ function selecionaBebida(i, preco, item){
     }
     elementos[x].classList.remove("escondido");
     icon[x].classList.remove("escondido");
-    flag+=1;
     precos[2] = Number(preco);
     nomes[2] = item;
     fecharPedido();
@@ -44,21 +41,27 @@ function selecionaSobremesa(i, preco, item){
         icon[j].classList.add("escondido");  
     }
     elementos[x].classList.remove("escondido");
-    flag+=1;
+    icon[x].classList.remove("escondido");
     precos[3] = Number(preco);
     nomes[3] = item;
     fecharPedido();
 }
 function fecharPedido(){
-    if(flag >= 3){
+    if(nomes[1] && nomes[2] && nomes[3]){
         document.querySelector(".bot button:nth-child(2)").classList.remove("escondido");
     }
     total = precos[1] + precos[2] + precos[3];
-    console.log(total);
+    total = total.toFixed(2);
 }
 function fazerPedido(){
-    /*let url =  encodeURIComponent("Olá, gostaria de fazer o pedido:- Prato: ${nomes[1]} - Bebida: ${nomes[2]}- Sobremesa: ${nomes[3]} Total: ${total}");
-    window.open(url).focus();*/
+    let url = `Olá, gostaria de fazer o pedido: 
+    - Prato: ${nomes[1]} 
+    - Bebida: ${nomes[2]} 
+    - Sobremesa: ${nomes[3]} 
+    Total: ${total} `;
+
+    url = encodeURIComponent(url);
+    window.open("https://wa.me/?text="+url);
 }
 
 
